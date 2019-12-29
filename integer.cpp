b35dde8314817573c9b5598f056c0e8d5772e675
jongcircle <alpha1490@naver.com>
2019-12-30 01:28:46 +0900

    .

--- integer.cpp
+++ integer.cpp
@@ -36,12 +36,9 @@ int main(){
     }
     
 
-
-
 }
 
 void add(string &int1, string &int2, string &result){
-    
     int size = max(int1.size(), int2.size());
     int* a = new int[size];
     int* b = new int[size];
@@ -72,11 +69,91 @@ void add(string &int1, string &int2, string &result){
 }
 
 void sub(string &int1, string &int2, string &result){
+    int size = max(int1.size(), int2.size());
+    int* a = new int[size];
+    int* b = new int[size];
+    int* res = new int[size];
+    // 크기 비교 아직 안됨
+    for (int i = 0; i < int1.size(); i++)
+    {
+        a[i] = int1.at(int1.size()-i-1) - '0';
+    }
+    for (int i = 0; i < int2.size(); i++)
+    {
+        b[i] = int2.at(int2.size()-i-1) - '0';
+    }
 
+    // int1이 더 클 때
 
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] > b[i])
+        {
+            res[i] = a[i] - b[i];
+        }
+        else if (a[i] < b[i]){
+            int temp = i;
+            while (1){
+                a[temp]--;
+                temp++;
+                if (a[temp] > b[temp]) break;
+            }
+            a[i] += 10;
+            res[i] = a[i] - b[i];
+        }
+        else {
+            res[i] = 0;
+        }
+        
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        cout << result << endl;
+        if(i == 0 && res[size] == 0) continue;
+        result += to_string(res[size-i]);
+    }
 
 }
 
 void mul(string &int1, string &int2, string &result){
+    int size = max(int1.size(), int2.size());
+    int* a = new int[size];
+    int* b = new int[size];
+    int* res = new int[size*2];
+    for (int i = 0; i < int1.size(); i++)
+    {
+        a[i] = int1.at(int1.size()-i-1) - '0';
+    }
+    for (int i = 0; i < int2.size(); i++)
+    {
+        b[i] = int2.at(int2.size()-i-1) - '0';
+    }
+    // int2가 짧다는 가정
+    for (int i = 0; i < int1.size(); i++)
+    {
+        for (int j = 0; j < int2.size(); j++)
+        {
+            int re;
+            re = a[j] * b[i];
+            res[j] = re%10;
+            res[i+1] += re/10;
+            if (res[i+1] >= 10){
+                int temp = i+1;
+                while (1){
+                    res[temp]++;
+                    temp++;
+                    if (res[temp] < 10) break;
+                }
+            }
+        }
+    }
+    
+    for (int i = 0; i < size*2; i++)
+    {
+        cout << result << endl;
+        if(i == 0 && res[size] == 0) continue;
+        result += to_string(res[size-i]);
+    }
 
 }
